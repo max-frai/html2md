b43ad17fb8f2cb3fe257433248ba19df9d556455
42 <me@maxfrai.com>
2020-06-27 19:38:10 +0300

    Dont put markdown chars after header

--- src/headers.rs
+++ src/headers.rs
@@ -1,7 +1,7 @@
-use super::TagHandler;
 use super::StructuredPrinter;
+use super::TagHandler;
 
-use markup5ever_rcdom::{Handle,NodeData};
+use markup5ever_rcdom::{Handle, NodeData};
 
 #[derive(Default)]
 pub(super) struct HeaderHandler {
@@ -9,11 +9,10 @@ pub(super) struct HeaderHandler {
 }
 
 impl TagHandler for HeaderHandler {
-
     fn handle(&mut self, tag: &Handle, printer: &mut StructuredPrinter) {
         self.header_type = match tag.data {
             NodeData::Element { ref name, .. } => name.local.to_string(),
-            _ => String::new()
+            _ => String::new(),
         };
 
         printer.insert_newline();
@@ -31,10 +30,10 @@ impl TagHandler for HeaderHandler {
         match self.header_type.as_ref() {
             "h1" => printer.append_str("\n==========\n"),
             "h2" => printer.append_str("\n----------\n"),
-            "h3" => printer.append_str(" ###\n"),
-            "h4" => printer.append_str(" ####\n"),
-            "h5" => printer.append_str(" #####\n"),
-            "h6" => printer.append_str(" ######\n"),
+            // "h3" => printer.append_str(" ###\n"),
+            // "h4" => printer.append_str(" ####\n"),
+            // "h5" => printer.append_str(" #####\n"),
+            // "h6" => printer.append_str(" ######\n"),
             _ => {}
         }
         printer.insert_newline();
